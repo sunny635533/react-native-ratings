4d84a48052b00f2392f4c1dae05d01499ea1d806
sunny <1604587251@qq.com>
2018-08-07 17:44:49 +0800

    add d.ts

---
+++ index.d.ts
@@ -0,0 +1,42 @@
+declare module 'react-native-ratings' {
+  import * as React from 'react';
+  import {
+    ViewStyle,
+    TextStyle,
+    ImageURISource
+  } from 'react-native';
+
+  export interface RatingProps extends React.Props<Rating> {
+    style?: ViewStyle;
+    type?: string;
+    startingValue?: number,
+    ratingImage?: ImageURISource;
+    ratingColor?: string,
+    ratingBackgroundColor?: string,
+    ratingCount?: number,
+    imageSize?: number,
+    onFinishRating?: (rating: number) => void,
+    showRating?: boolean,
+  }
+
+  export class Rating extends React.Component<RatingProps>  {
+
+  }
+
+  export interface AirbnbRatingProps extends React.Props<AirbnbRating> {
+    style?: ViewStyle;
+    count?: number;
+    size?:number;
+    defaultRating?: number;
+    reviews?: string[]
+    onFinishRating?: (rating: number) => void,
+    showRating?: boolean,
+  }
+
+  export class AirbnbRating extends React.Component<AirbnbRatingProps>  {
+
+  }
+
+
+}
+
